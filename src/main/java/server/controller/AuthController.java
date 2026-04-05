3965828ba67336299326d5b8d03945c62b5a34b6
Cobin <halfkey.studio.games@gmail.com>
2026-04-04 20:27:33 -0400

    feat: Created register() method in AuthController

--- src/main/java/server/controller/AuthController.java
+++ src/main/java/server/controller/AuthController.java
@@ -1,5 +1,6 @@
 package server.controller;
 
+import com.google.firebase.auth.FirebaseAuthException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import server.model.LoginRequest;
@@ -23,5 +24,18 @@ public class AuthController {
             return ResponseEntity.ok(response);
         }
         return ResponseEntity.status(401).body(response);
+    }
+
+    //Method to help create the account on the createAccount page
+    @PostMapping("/register")
+    public ResponseEntity<String> register(@RequestBody LoginRequest request)
+    {
+        try {
+            authService.createAccount(request.getUsername(), request.getPassword(), "First", "Last");
+
+            return ResponseEntity.ok("User Created Successfully");
+        } catch (FirebaseAuthException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
